26d36fe7e8c9d3b56db00f46eacde68f6fa2867f
lalinda74 <diaslalinda@gmail.com>
2022-02-02 20:44:20 +0530

    UI changes

--- src/components/page-sections/pricing-plans/index.js
+++ src/components/page-sections/pricing-plans/index.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { Container } from "react-bootstrap";
 import DoneIcon from '../../../../public/static/Done.svg';
+import LocationImg from '../../../images/location.svg';
+import DeliveryImg from '../../../images/delivery.png';
 
 const PricingPlans = () => {
   return (
@@ -14,54 +16,43 @@ const PricingPlans = () => {
         you’ll pay.
       </p>
       <div className="row py-5 rs-card-wrapper">
-        <div className="col-12 col-sm-4 col-md-4 mb-5">
+        <div className="col-12 col-sm-4 offset-md-2 col-md-4 mb-5">
           <div className="rs-card">
             <div>
-              <h3 className="text-center">Basic</h3>
-              <p className="text-center">Made for Starters</p>
+              <div className="d-flex justify-content-center">
+                <img src={LocationImg} className="rs-card__img text-center" />
+              </div>
+              <h3 className="text-center pt-4">Pay for location usage</h3>
+              {/* <p className="text-center">Made for Starters</p> */}
               <ul className="pt-3 rs-card__list">
-                <li className="rs-card__list-item"><img src={DoneIcon} alt = 'Done Icon' /> 1,000 locations</li>
-                <li className="rs-card__list-item"><img src={DoneIcon} alt = 'Done Icon' /> Mail support</li>
+                <li className="rs-card__list-item"><img src={DoneIcon} alt = 'Done Icon' /> Starting at &nbsp;<strong>$29.99</strong></li>
+                <li className="rs-card__list-item"><img src={DoneIcon} alt = 'Done Icon' /> 1,000 Locations</li>
               </ul>
             </div>
-            <div>
+            {/* <div>
               <h4 className="text-center">LKR 35,000 / month</h4>
               <p className="text-center">LKR 8 per additional request</p>
-            </div>
+            </div> */}
           </div>
         </div>
         <div className="col-12 col-sm-4 col-md-4 mb-5">
           <div className="rs-card">
-            <span className="rs-card__tag">Popular</span>
+            {/* <span className="rs-card__tag">Popular</span> */}
             <div>
-              <h3 className="text-center">Pro</h3>
-              <p className="text-center">Made for Growth</p>
+            <div className="d-flex justify-content-center">
+                <img src={DeliveryImg} className="rs-card__img text-center" />
+              </div>
+              <h3 className="text-center pt-4">Pay per vehicle per month</h3>
+              {/* <p className="text-center">Made for Growth</p> */}
               <ul className="pt-3">
-                <li className="rs-card__list-item"><img src={DoneIcon} alt = 'Done Icon' /> 10,000 locations</li>
-                <li className="rs-card__list-item"><img src={DoneIcon} alt = 'Done Icon' /> Mail and phone support</li>
+                <li className="rs-card__list-item"><img src={DoneIcon} alt = 'Done Icon' /> Starting at &nbsp;<strong>$49.99</strong></li>
+                <li className="rs-card__list-item"><img src={DoneIcon} alt = 'Done Icon' /> With live tracking &nbsp;<strong>$69.99</strong></li>
               </ul>
             </div>
-            <div>
+            {/* <div>
               <h4 className="text-center">LKR 60,000 / month</h4>
               <p className="text-center">LKR 7 per additional request</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-12 col-sm-4 col-md-4 mb-5">
-          <div className="rs-card">
-            <div>
-              <h3 className="text-center">Enterprise</h3>
-              <p className="text-center">Made for Corporate</p>
-              <ul className="pt-3">
-                <li className="rs-card__list-item"><img src={DoneIcon} alt = 'Done Icon' /> 25,000 locations</li>
-                <li className="rs-card__list-item"><img src={DoneIcon} alt = 'Done Icon' /> Mail and phone support</li>
-                <li className="rs-card__list-item"><img src={DoneIcon} alt = 'Done Icon' /> Advanced analytics dashboard</li>
-              </ul>
-            </div>
-            <div>
-              <h4 className="text-center">LKR 150,000 / month</h4>
-              <p className="text-center">LKR 6 per additional request</p>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
